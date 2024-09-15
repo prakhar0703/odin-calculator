4d586210a270d77178e9a21246ed4bb9ceb48a59
Prakhar <dixit.madhur.07@gmail.com>
2024-09-15 23:19:02 +0530

    Initial setup for the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# odin-calculator
+#Odin Calculator
 The last project in The Odin Project foundations.
+A very basic calculator.
---
+++ script.js
@@ -0,0 +1,15 @@
+function add(operand1, operand2){
+    return operand1 + operand2;
+}
+
+function subtract(operand1, operand2){
+    return operand1 - operand2;
+}
+
+function multiply(operand1, operand2){
+    return operand1 * operand2;
+}
+
+function divide(operand1, operand2){
+    return operand1 / operand2;
+}
